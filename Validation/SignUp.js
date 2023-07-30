1dc86492d65b0838b272adae4f83ebcfa3d7be26
Kavinkumar V N <116902936+kavinkumar1510@users.noreply.github.com>
2023-07-30 13:02:44 +0530

    Add files via upload

---
+++ Validation/SignUp.js
@@ -0,0 +1,38 @@
+function valid() {
+    let name = document.getElementById("name").value.trim();
+    let email = document.getElementById("email").value;
+    let pass1 = document.getElementById("pass1").value;
+    let pass2 = document.getElementById("pass2").value;
+    let num = document.getElementById("num").value;
+
+    const phn = /^\d{10}$/;
+    const em = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+    if (name === "") {
+        alert("Username can't be empty");
+        return false;
+    }
+
+    if (num === "" || !phn.test(num)) {
+        alert("Enter a valid PhoneNumber");
+        return false;
+    }
+
+    if (email === "" || !em.test(email)) {
+        alert("Enter a valid email");
+        return false;
+    }
+
+    if (pass1.length < 8) {
+        alert("The password length should be 8 or more than 8");
+        return false;
+    }
+
+    if (pass1 !== pass2) {
+        alert("The password doesn't match");
+        return false;
+    }
+
+    alert("SignedUp Successfully");
+    return true;
+}
